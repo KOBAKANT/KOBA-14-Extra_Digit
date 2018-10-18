aff0e6c064f9f2cb60895f2dd526cada07903b1d
mikst <mikst@mikst.fritz.box>
2018-10-18 23:13:26 +0200

    2nd prototype code

--- Sensor_display/Sensor_display.ino
+++ Sensor_display/Sensor_display.ino
@@ -1,9 +1,26 @@
 #include <MicroView.h>
-
+#include <Servo.h>
 
 int val;
 int x[64];
 int dispVal;
+
+int angle;
+int Avg;
+int sampleSize = 5;
+
+Servo myservo;
+
+
+// adjust the following number--------
+
+int bendSensor_max = 850;
+int bendSensor_min = 750;
+
+int motorPos_max = 170;  // streight position. 180 max
+int motorPos_min = 20;  // the smaller number the bigger bend angle
+
+//------------------------------------
 
 
 void setup() {
@@ -14,12 +31,16 @@ void setup() {
   uView.display();
 
   // read analog input pin
-  val = analogRead(A0);
+  val = analogRead(A2);
   // map the value to y value of the graph. first 1-10 pixel is taken for number display
   dispVal = map(val, 0, 1023, 48, 10);
-   for (int i = 0; i < 63; i++) {
+  for (int i = 0; i < 63; i++) {
     x[i] = dispVal;
   }
+
+  // set initial avarage and angle
+  angle = map(val, 0, 1023, 0, 180);
+  myservo.attach(6);
 
   delay(1000);
 
@@ -31,15 +52,39 @@ void loop () {
 
   // clear the display
   uView.clear(PAGE);
-  
+
   // read analog input pin
-  val = analogRead(A0);
+  val = analogRead(A2);
+  // reverse the reading and scale
+  val = map (val, 1023, 0, 0, 1023);
+
+// take avarage of sample size. currently disabled
+//  Avg = (val + Avg * (sampleSize - 1)) / sampleSize;
+//  val = Avg;
+//----------------------------
+ 
+
+  //------------------------------
+  // motor control related----
+  //------------------------------
+  // map the bend sensor value to the motor position
+  angle = map(val, bendSensor_max, bendSensor_min, motorPos_min, motorPos_max);
+  angle = constrain(angle,  motorPos_min, motorPos_max);
+
+  // control the servo motor
+  myservo.write(angle);
+
+   //------------------------------
+  // microview display related----
+  //------------------------------
   // map the value to y value of the graph. first 1-10 pixel is taken for number display
   dispVal = map(val, 0, 1023, 48, 10);
 
   // sent the number display position to 0,0
   uView.setCursor(0, 0);
   uView.print(val);  // display val
+  uView.setCursor(0, 10);
+  uView.print(angle);  // display val
 
   // microview display resolution is 64x48
   for (int i = 0; i < 63; i++) {
@@ -54,8 +99,11 @@ void loop () {
   // show on the display
   uView.display();
 
-  // print to Serial with Plotter so we can see the sensor value
-  serialPlotterView(val);
+  //------------------------------
+
+  // print to Serial so we can see the sensor value
+  //serialPlotterView(val);
+  serialPrintDebugger();
 
   delay(20);
 }
@@ -66,5 +114,13 @@ void serialPlotterView(int num) {
   Serial.print(0);
   Serial.print(" , ");
   Serial.println(num);
+}
+
+void serialPrintDebugger() {
+  Serial.print("sensorVal; ");
+  Serial.print(val);
+  Serial.print("\t motorAngle; ");
+  Serial.print(angle);
+  Serial.println();
 }
 
